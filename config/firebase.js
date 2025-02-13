2354efb289f7488bcb530218867a8f80aebab6a0
Rahul Jalandhra <jalandhra91256@gmaill.com>
2025-02-13 14:55:24 +0530

    firebase config removed

--- config/firebase.js
+++ config/firebase.js
@@ -1,8 +1,11 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./esferasoft-test-firebase-adminsdk-fbsvc-c2b6634455.json");
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: {
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY
+    },
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET, // Firebase Storage Bucket URL
 });
 
