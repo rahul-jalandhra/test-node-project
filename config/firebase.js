38401e305902a8cc97cc06b1d924f4a5d7aaa863
Rahul Jalandhra <jalandhra91256@gmaill.com>
2025-02-13 15:24:56 +0530

    firebase issue fixed

--- config/firebase.js
+++ config/firebase.js
@@ -1,11 +1,11 @@
 const admin = require("firebase-admin");
 
 admin.initializeApp({
-    credential: {
+    credential:  admin.credential.cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
         privateKey: process.env.FIREBASE_PRIVATE_KEY
-    },
+    }),
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET, // Firebase Storage Bucket URL
 });
 
